0ade5e95d62ba4e77546a795024a677035582676
Aseel-prog911 <aseelkhalil741@gmail.com>
2026-07-08 01:27:00 +0400

    Task3  Multiplication Table

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -27,6 +27,15 @@
             }
 
             Console.WriteLine("Sum = " + sum);
+            ////////////////////////////////////////////
+            //Task3  Multiplication Table
+            Console.Write("Enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(number + " x " + i + " = " + (number * i));
+            }
         }
     }
 }
