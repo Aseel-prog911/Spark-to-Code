37ed10f7a36fd1c6c641ca007ddf4af9d4360432
Aseel-prog911 <aseelkhalil741@gmail.com>
2026-07-08 02:17:58 +0400

    Task 3 - Celsius to Fahrenheit Function

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -27,6 +27,19 @@
             int result = Square(number);
 
             Console.WriteLine("Square = " + result);
+            ///////////////////////////////////////////////
+            //Task 3 - Celsius to Fahrenheit Function
+            double CelsiusToFahrenheit(double celsius)
+            {
+                return (celsius * 9 / 5) + 32;
+            }
+
+            Console.Write("Enter temperature in Celsius: ");
+            double celsius = Convert.ToDouble(Console.ReadLine());
+
+            double fahrenheit = CelsiusToFahrenheit(celsius);
+
+            Console.WriteLine("Fahrenheit = " + fahrenheit);
         }
     }
 }
