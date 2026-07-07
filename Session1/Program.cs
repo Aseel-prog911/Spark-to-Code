8c986d1e15cbd172871afae80952956dbc1b78f0
Aseel-prog911 <aseelkhalil741@gmail.com>
2026-07-08 01:02:01 +0400

    Task6 Temperature Converter

--- Session1/Program.cs
+++ Session1/Program.cs
@@ -91,6 +91,27 @@
                     break;
             }
             ///////////////////////////////////////////
+            //Task6 Temperature Converter
+            Console.Write("Enter temperature in Celsius: ");
+            double celsius = Convert.ToDouble(Console.ReadLine());
+
+            double fahrenheit = (celsius * 9 / 5) + 32;
+
+            Console.WriteLine("Fahrenheit = " + fahrenheit);
+
+            if (celsius < 10)
+            {
+                Console.WriteLine("Weather: Cold");
+            }
+            else if (celsius <= 30)
+            {
+                Console.WriteLine("Weather: Mild");
+            }
+            else
+            {
+                Console.WriteLine("Weather: Hot");
+            }
+            /////////////////////////////////////////////////
         }
     }
 }
