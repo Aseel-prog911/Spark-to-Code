d4542ac55358f3354f49f90568037523fc5891e4
Aseel-prog911 <aseelkhalil741@gmail.com>
2026-07-08 02:48:18 +0400

    Task 1 - Fixed Grades Array

---
+++ Task5/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Task5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task 1 - Fixed Grades Array
+            int[] grades = new int[5];
+
+            for (int i = 0; i < grades.Length; i++)
+            {
+                Console.Write("Enter grade " + (i + 1) + ": ");
+                grades[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("Grades:");
+
+            foreach (int grade in grades)
+            {
+                Console.WriteLine(grade);
+            }
+        }
+    }
+}
