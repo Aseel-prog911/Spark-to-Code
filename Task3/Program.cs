587049e6b8f022fa0794e618bdfef299d85942a5
Aseel-prog911 <aseelkhalil741@gmail.com>
2026-07-08 01:44:15 +0400

    Task 1 - Absolute Difference

---
+++ Task3/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task 1 - Absolute Difference
+            Console.Write("Enter the first number: ");
+            double firstNumber = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter the second number: ");
+            double secondNumber = Convert.ToDouble(Console.ReadLine());
+
+            double difference = Math.Abs(firstNumber - secondNumber);
+
+            Console.WriteLine("Positive Difference = " + difference);
+        }
+    }
+}
