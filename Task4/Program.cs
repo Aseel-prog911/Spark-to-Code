e7e59dfb0665892da438caf0ef957ee587d7150e
Aseel-prog911 <aseelkhalil741@gmail.com>
2026-07-08 02:14:37 +0400

    Task 1 - Personalized Welcome Function

---
+++ Task4/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task 1 - Personalized Welcome Function
+            void PrintWelcome(string name)
+            {
+                Console.WriteLine("Welcome, " + name + "!");
+            }
+
+            Console.Write("Enter your name: ");
+            string userName = Console.ReadLine();
+
+            PrintWelcome(userName);
+        }
+    }
+}
