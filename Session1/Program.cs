58802f47d78e30f2410eff20c8460a842a6487df
Aseel-prog911 <aseelkhalil741@gmail.com>
2026-07-08 01:00:44 +0400

    Task5  Grade Letter Lookup

--- Session1/Program.cs
+++ Session1/Program.cs
@@ -39,27 +39,58 @@
             else
             {
                 Console.WriteLine("The number is Odd.");
-                //////////////////////////////////////////////////
-                //Task4  Voting Eligibility
-                Console.Write("Enter your age: ");
-                int age = Convert.ToInt32(Console.ReadLine());
-
-                Console.Write("Do you have a valid national ID? (yes/no): ");
-                string answer = Console.ReadLine();
-
-                bool hasID = answer.ToLower() == "yes";
-
-                if (age >= 18 && hasID)
-                {
-                    Console.WriteLine("You are eligible to vote.");
-                }
-                else
-                {
-                    Console.WriteLine("You are not eligible to vote.");
-                }
-
+                
             }
+            //////////////////////////////////////////////////
+            //Task4  Voting Eligibility
+            Console.Write("Enter your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
 
+            Console.Write("Do you have a valid national ID? (yes/no): ");
+            string answer = Console.ReadLine();
+
+            bool hasID = answer.ToLower() == "yes";
+
+            if (age >= 18 && hasID)
+            {
+                Console.WriteLine("You are eligible to vote.");
+            }
+            else
+            {
+                Console.WriteLine("You are not eligible to vote.");
+            }
+            /////////////////////////////////////////////
+            //Task5  Grade Letter Lookup
+            Console.Write("Enter grade letter (A, B, C, D, F): ");
+            char grade = Convert.ToChar(Console.ReadLine());
+
+            switch (grade)
+            {
+                case 'A':
+                    Console.WriteLine("Excellent");
+                    break;
+
+                case 'B':
+                    Console.WriteLine("Very Good");
+                    break;
+
+                case 'C':
+                    Console.WriteLine("Good");
+                    break;
+
+                case 'D':
+                    Console.WriteLine("Pass");
+                    break;
+
+                case 'F':
+                    Console.WriteLine("Fail");
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid grade");
+                    break;
+            }
+            ///////////////////////////////////////////
         }
     }
 }
