9d993e0e1055de620064a2b134ff1f48912c05ec
Aseel-prog911 <aseelkhalil741@gmail.com>
2026-07-08 01:20:42 +0400

    Task1 Countdown Timer

---
+++ Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task1 Countdown Timer
+            Console.Write("Enter the starting number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = number; i >= 1; i--)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("Liftoff!");
+        }
+    }
+}
