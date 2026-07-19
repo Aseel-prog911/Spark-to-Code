ac6aa79f4d0ff1f650c2c2df6d17e7dac513df24
Aseel-prog911 <aseelkhalil741@gmail.com>
2026-07-20 02:44:26 +0400

    add Guest class with displayGuest() and calculateTotalCost() methods

---
+++ Hotel Management System/Guest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace HotelManagementSystem
+{
+    class Guest
+    {
+        public string guestId;
+        public string guestName;
+        public string roomNumber;
+        public string checkInDate;
+        public int totalNights;
+
+        public Guest(string guestId, string guestName, string checkInDate, int totalNights, string roomNumber = "Not Assigned")
+        {
+            this.guestId = guestId;
+            this.guestName = guestName;
+            this.roomNumber = roomNumber;
+            this.checkInDate = checkInDate;
+            this.totalNights = totalNights;
+        }
+
+        public void displayGuest()
+        {
+            Console.WriteLine($"ID: {guestId} | Name: {guestName} | Room: {roomNumber} | Check-in: {checkInDate} | Nights: {totalNights}");
+        }
+
+        public double calculateTotalCost(double pricePerNight)
+        {
+            return pricePerNight * totalNights;
+        }
+    }
+}
